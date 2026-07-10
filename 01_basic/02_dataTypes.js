0497ab0c8c403b517d4767a291bffc5867788e6e
Dabhi Harsh Maheshbhai <23bmiit018@gmail.com>
2026-07-10 14:51:39 +0000

    Data Types done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat as a newer version of JS
+console.log(3+3)
+
+let name = "John" //string
+let age = 30 //number
+let isMarried = false //boolean
+let city; //undefined
+
+console.log("=========")
+console.log(name)
+console.log(age)
+console.log(isMarried)
+
+//Data types in JS
+console.log("=========")
+console.log(typeof name); //want to know the variables data type
+console.log(typeof age);
+
+/*
+number => represents both integer and floating point numbers
+string => represents a sequence of characters, enclosed in single or double quotes
+boolean => represents a logical value, either true or false
+null => represents the intentional absence of any object value
+undefined => represents a variable that has been declared but not assigned a value
+object => represents a collection of key-value pairs, used to store complex data structures
+symbol => represents a unique and immutable value, often used as an identifier for object properties
+*/
