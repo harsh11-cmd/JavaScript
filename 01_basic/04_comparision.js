3f89d5b39e0655368e31839f6c0c633ac6b333cd
Dabhi Harsh Maheshbhai <23bmiit018@gmail.com>
2026-07-11 09:17:42 +0000

    Comparision Done

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,18 @@
+/*
+> : Greater than
+>= : Greater than or equal to
+< : Less than
+<= : Less than or equal to
+== : Equal to
+!= : Not equal to
+=== : Equal value and equal datatype
+*/
+console.log(10>5) //output: true
+console.log(10>=5) //output: true
+console.log(10<5) //output: false
+console.log(10<=5) //output: false
+console.log(10==5) //output: false
+console.log(10!=5) //output: true
+console.log(10===5) //output: false
+console.log(10===10) //output: true
+console.log(10==="10") //output: false
