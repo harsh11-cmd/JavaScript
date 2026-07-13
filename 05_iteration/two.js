74d74f55a0d2ad8e612ef5e8ef1d45b93dd2c479
Dabhi Harsh Maheshbhai <23bmiit018@gmail.com>
2026-07-13 10:33:57 +0000

    While & do-while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+//While loop
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     //i++;
+//     i+=2; // it will increment the value of i by 2 in each iteration
+// }
+
+//Do while loop
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+let i=11;
+do{
+    console.log(i);
+    i++;
+}while(i<=10); // it will execute the block of code once even if the condition is false because the condition is checked after the block of code is executed.
