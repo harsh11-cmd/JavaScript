b17045f3386d40b7cbc1d68813bbfc72b47fe958
Dabhi Harsh Maheshbhai <23bmiit018@gmail.com>
2026-07-13 12:36:56 +0000

    reduce()

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -1,1 +1,18 @@
 // reduce() prototype method executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
+const mynyms = [1, 2, 3, 4, 5];
+
+const sum=mynyms.reduce(function (accumulator,currentValue){
+    console.log(`accumulator: ${accumulator}, currentValue: ${currentValue}`);
+        return accumulator+currentValue;
+},0);
+const sum2=mynyms.reduce((accumulator,currentValue)=>accumulator+currentValue,0);
+console.log(sum); // output: 15 
+console.log(sum2); // output: 15
+
+// Why output is 15 because reduce() method executes a reducer function (that you provide) on each element of the array, 
+// resulting in a single output value. The first parameter of the reducer function is the accumulator, 
+// which accumulates the callback's return values. The second parameter is the currentValue, 
+// which is the current element being processed in the array. The second argument to reduce() (0 in this case) is 
+// the initial value of the accumulator.
+
+
