a30bd8ba4a87f189fc30dd123364481ad744e187
Dabhi Harsh Maheshbhai <23bmiit018@gmail.com>
2026-07-13 09:43:56 +0000

    If - else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+//If statement
+
+// const temp=40;
+// if(temp>20)
+// {
+//     console.log("It is a hot day");
+// }
+// else
+// {
+//     console.log("It is a cold day");
+// }
+
+let score = 100;
+if(score>90)
+{
+    let power="fly";
+    console.log(`You have power to ${power}`);
+}
+//Also we can use else if statement to check multiple conditions
+// check multiple conditions in one if using  && operator (both conditions should be true) and || operator (either condition should be true)
