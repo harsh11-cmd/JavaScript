9be314020ffff6c0c2d7a6d562c684b4c4ee862a
Dabhi Harsh Maheshbhai <23bmiit018@gmail.com>
2026-07-13 10:05:31 +0000

    Switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = 3;
+switch (month){
+    case 1: month==1
+    console.log("January");
+    break;
+    case 2: month==2
+    console.log("February");
+    break;
+    case 3: month==3
+    console.log("March");
+    break;
+    case 4: month==4
+    console.log("April");
+    break;
+    case 5: month==5
+    console.log("May");
+    break;  
+    default: console.log("Invalid month");            
+}
